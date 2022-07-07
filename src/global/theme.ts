644af67204f5b6f77bba6b434f455a3faebab704
eds9nsilva <antonioedson99@gmail.com>
2022-07-07 16:29:24 -0300

    feat: create style global

---
+++ src/global/theme.ts
@@ -0,0 +1,17 @@
+export default {
+  colors: {
+    blue: '#090F24',
+    black: '#000',
+    white: '#fff',
+    primary: '#fee438',
+    success: '#12a454',
+    danger: '#e83f5b',
+    dark: '#121214',
+    gray500: '#a8a8b3',
+    gray800: '#29292e',
+  },
+  fonts: {
+    regular: 'Roboto_400Regular',
+    bold: 'Roboto_700Bold',
+  },
+};
